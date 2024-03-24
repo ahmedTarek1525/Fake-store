6d3e22535309ea3d62a63cd9997dae6735c66d07
Ahmed Tarek <160412976+ahmedTarek1525@users.noreply.github.com>
2024-03-24 08:14:17 +0200

    add the removeSelectedProduct reducer. fix bug of the appearing previous selected product.

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -2,7 +2,7 @@ import React , {useEffect} from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { selectedProduct } from "../redux/actions/productActions";
+import { selectedProduct, removeSelectedProduct } from "../redux/actions/productActions";
 
 const ProductDetail = () => {
     const product = useSelector((state) => state.product);
@@ -20,6 +20,9 @@ const ProductDetail = () => {
     };
     useEffect(() => {
         if (productId && productId !== "") fetchProductDetail(productId);
+        return () => {
+            dispatch(removeSelectedProduct());
+        };
     },[productId]);
 
     return(
